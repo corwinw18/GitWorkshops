7ccf899ffba79475266076a4034b3ec55c1059a4
ranger6777 <ranger6777@gmail.com>
2018-09-26 19:36:09 -0400

    ( ͡° ͜ʖ ͡°)

--- MergeConflict.py
+++ MergeConflict.py
@@ -4,4 +4,5 @@
 # Each person should change to following line of code:
 
 print("This was initially just a simple sentence, but now I made it look like code. It doesn't actually matter, but it'll look more familiar this way");
-print("Hello world, my name is <NAME_HERE> and my favorite programming language is <LANGUAGE_HERE> ");
+print("Hello world, my name is Michael and my favorite programming language is Machine Code");
+print("Somebody upload the lyrics to darude sandstorm")
